db8ef256f5c987d25254ef95c3e487fc53f2e776
emanuelromeo <emanuel.romeo@hotmail.com>
2024-11-26 18:21:56 +0100

    Deleted DessertMenu.java, added GestioneMenuRistoranteApp.java and updated Menu.java

---
+++ src/GestioneMenuRistoranteApp.java
@@ -0,0 +1,9 @@
+public class GestioneMenuRistoranteApp {
+    public static void main(String[] args) {
+
+        //Istanziamento e stampa del Menù
+        Menu menu = new Menu();
+        System.out.println(menu);
+
+    }
+}
